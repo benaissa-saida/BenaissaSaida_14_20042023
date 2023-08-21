c576660a9c4444149958771046b5bc37aafd1762
benaissa-saida <benaissasaida28@gmail.com>
2023-08-21 13:29:47 +0200

    dataTable added

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,18 +1,27 @@
 import { Link } from "react-router-dom";
 import { EmployeesContext } from "../components/SaveEmployees";
 import { useContext } from "react";
+import DataTable from "mik-datatable/dist/dataTable";
 
 function Employees() {
   const employeesContext = useContext(EmployeesContext);
   const { employeesList } = employeesContext;
+  const headers = [
+    { name: "First Name", field: "firstName" },
+    { name: "Last Name", field: "lastName" },
+    { name: "Start Date", field: "startDate" },
+    { name: "Department", field: "department" },
+    { name: "Date of Birth", field: "dateOfBirth" },
+    { name: "Street", field: "street" },
+    { name: "City", field: "city" },
+    { name: "State", field: "state" },
+    { name: "Zip Code", field: "zipCode" },
+  ];
 
   return (
     <div className="container">
       <h1>Current Employees</h1>
-      {employeesList.length}
-      {employeesList.map((employee) => (
-        <div key={employee.id}>{employee.firstName}</div>
-      ))}
+      <DataTable data={employeesList} headers={headers} />
       <Link to="/">Home</Link>
     </div>
   );
